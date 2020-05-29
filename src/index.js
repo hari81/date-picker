2455f5dc9c6d37d7e1769a037405399199bc01bc
Hari Suddapalli <hk.sudda@gmail.com>
2020-05-29 11:57:19 +1000

    date picker added

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axe from 'react-axe';
+import App from './app';
+
+if (process.env.NODE_ENV !== 'production') {
+  axe(React, ReactDOM, 1000);
+}
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
